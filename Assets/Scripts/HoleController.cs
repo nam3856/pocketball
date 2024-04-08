037568d3e60422687963b7aefc795730b5c15bd7
kinggodmini <nam3856@gmail.com>
2024-04-08 18:56:39 +0900

    Hole Fix2
    테이블 마찰력 올리고 공 조금 이따 없애게 수정

--- Assets/Scripts/HoleController.cs
+++ Assets/Scripts/HoleController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,10 +8,22 @@ public class HoleController : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log($"{other.gameObject.name} 진입");
         if (other.CompareTag("Ball") && other.gameObject.name.CompareTo("Cue Ball") != 0)
         {
-            Debug.Log($"{other.gameObject.name} 빠짐");
-            Destroy(other.gameObject);
+            StartCoroutine(ByeBye(other));
         }
+    }
+    private IEnumerator ByeBye(Collider other)
+    {
+        Debug.Log($"{other.gameObject.name} 빠짐");
+        // 1초 기다림
+        yield return new WaitForSeconds(1f);
+        other.gameObject.GetComponent<SphereCollider>().enabled = false;
+        other.gameObject.GetComponent<Rigidbody>().mass = 0.2f;
+        // 2초 기다림
+        yield return new WaitForSeconds(2f);
+        // 오브젝트 제거
+        Destroy(other.gameObject);
     }
 }
