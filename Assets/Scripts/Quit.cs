9d0bd521d08f7966f3794de0c319213348115a12
kinggodmini <nam3856@gmail.com>
2024-04-17 20:03:59 +0900

    게임종료 추가

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
