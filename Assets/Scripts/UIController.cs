0909d24139b1fe834bcbee0b6eee39563bef7412
kinggodmini <nam3856@gmail.com>
2024-11-07 21:27:40 +0900

    NetCode 패키지 임포트

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -11,18 +11,20 @@ public class UIController : MonoBehaviour
     void OnEnable()
     {
         GameManager.OnScoreChanged += UpdateScore;
+        GameManager.OnTurnChanged += UpdateTurnText;
     }
 
     void OnDisable()
     {
         GameManager.OnScoreChanged -= UpdateScore;
+        GameManager.OnTurnChanged -= UpdateTurnText;
     }
 
-    private void UpdateTurnText(string playerName)
+    private void UpdateTurnText(int playerName)
     {
         turnText.text = "Player "+ playerName + "'s Turn";
     }
     private void UpdateScore(int solid, int striped) {
-        score.text = "Player 1: " + solid + "\nPlayer 2: " + striped;
+        //score.text = "Player 1: " + solid + "\nPlayer 2: " + striped;
     }
 }
